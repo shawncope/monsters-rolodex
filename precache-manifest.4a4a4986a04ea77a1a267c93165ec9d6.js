1906376fb12c5d11960f4b3196fff5e89754daa2
Shawn Cope <scope@avantia-inc.com>
2019-10-09 11:53:30 -0400

    Updates

---
+++ precache-manifest.4a4a4986a04ea77a1a267c93165ec9d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044d58f631b609742cda2089322c48a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "def12a02e62456936646",
+    "url": "/monsters-rolodex/static/css/main.4f4c1014.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "def12a02e62456936646",
+    "url": "/monsters-rolodex/static/js/main.fab1d871.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
